8e62466432baf4f15d68a35a2778ca7f1b9c0b2f
oddedgeffen <odded9000@gmail.com>
2025-11-07 05:22:29 +0200

    deploy!

--- runpod_worker/handler.py
+++ runpod_worker/handler.py
@@ -1,16 +1,7 @@
 import runpod
 
 def handler(event):
-    """
-    Simple test handler
-    """
-    job_input = event.get('input', {})
-    
-    return {
-        "status": "success",
-        "message": "RunPod queue worker is running!",
-        "received_input": job_input
-    }
+    """Simple hello-world test."""
+    return {"message": "RunPod worker is alive odded!"}
 
-if __name__ == "__main__":
-    runpod.serverless.start({"handler": handler})
+runpod.serverless.start({"handler": handler})
